71ba521cfe17ed383a0642db4c2f8e60236920d3
SumSumCorn <a01035587180@gmail.com>
2022-02-08 17:53:18 +0900

    presentation, container 컴포넌트 생성

---
+++ src/containers/CounterContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { increase, decrease } from '../modules/counter';
+import Counter from '../components/Counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return <Counter number={1} onIncrease={() => {}} onDecrease={() => {}} />;
+};
+
+// export default connect(
+//   (state) => ({
+//     number: state.counter,
+//   }),
+//   {
+//     increase,
+//     decrease,
+//   },
+// )(CounterContainer);
+export default CounterContainer;
