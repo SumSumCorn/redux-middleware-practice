de2ae0a78fea97e05b51f078d79fc8deec5c04fb
SumSumCorn <a01035587180@gmail.com>
2022-02-08 17:41:58 +0900

    redux 적용전

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const store = createStore();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <Provider>
+  <App />,
+  // </Provider>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
